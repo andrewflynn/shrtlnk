2b34515635cb1181ae20862a7cebcc77bcdea7f4
Andrew Flynn <aflynn@gmail.com>
2016-07-09 12:01:15 -0400

    Better amazon regex
    Amazon places product names in random ways, so
    just check for the first 10 character word that begins
    with capital B

--- app/regex.js
+++ app/regex.js
@@ -1,6 +1,6 @@
 function shrtn(str) {
   // amazon: https://amzn.com/B01A6G35IQ
-  var amzn = /^http(?:s)?:\/\/(?:www\.)?amazon\.com\/gp\/product\/(\w+)\/?.*$/;
+  var amzn = /^http(?:s)?:\/\/(?:www\.)?amazon\.com\/[\w\/-]*(B\w{9}).*$/;
   if (amzn.test(str)) {
     return str.replace(amzn, 'https://amzn.com/$1');
   }
--- test/test.js
+++ test/test.js
@@ -13,3 +13,12 @@ QUnit.test("Amazon simple", function(assert) {
 QUnit.test("Amazon simple no trailing slash", function(assert) {
   assert.equal(shrtn("https://www.amazon.com/gp/product/B01A6G35IQ"), "https://amzn.com/B01A6G35IQ", "Passed");
 });
+QUnit.test("Amazon dp then product name", function(assert) {
+  assert.equal(shrtn("https://www.amazon.com/dp/B011KE4VE0/ref=nav_timeline_asin?_encoding=UTF8&psc=1"), "https://amzn.com/B011KE4VE0", "Passed");
+});
+QUnit.test("Amazon product name first", function(assert) {
+  assert.equal(shrtn("https://www.amazon.com/Etekcity-Ultralight-Portable-Backpacking-Ignition/dp/B00B4FY8YO?ie=UTF8&*Version*=1&*entries*=0"), "https://amzn.com/B00B4FY8YO", "Passed");
+});
+QUnit.test("Amazon product name first 2", function(assert) {
+  assert.equal(shrtn("https://www.amazon.com/Apple-iPhone-5S-16GB-Refurbished/dp/B00YD53YQU/ref=lp_12522860011_1_1?s=wireless&ie=UTF8&qid=1468079498&sr=1-1"), "https://amzn.com/B00YD53YQU", "Passed");
+});
